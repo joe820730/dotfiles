fe55877dbce4febd3a051e0fd36b62cbf71a3e51
Joe Tsai <joe.tsai8207@gmail.com>
2018-04-09 09:26:15 +0800

    Add install scripts.

---
+++ install.sh
@@ -0,0 +1,10 @@
+#~/bin/sh
+sudo apt install git vim tmux
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+mkdir ~/.vim
+mkdir ~/.vim/colors
+git clone https://github.com/kiddos/malikai.vim.git ~/.vim/malokai.vim
+ln -s ~/.vim/malokai.vim/colors/malokai.vim ~/.vim.colors/malokai.vim
+cd ~
+wget https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
+mv git-prompt.sh .git-prompt.sh
