6342d208ff60fbef6b3830ccf1d42830ebff9f58
Joe Tsai <joe.tsai8207@gmail.com>
2018-11-09 10:25:48 +0800

    Fix install.sh typo.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt install git vim tmux
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 mkdir ~/.vim
 mkdir ~/.vim/colors
-git clone https://github.com/kiddos/malikai.vim.git ~/.vim/malokai.vim
+git clone https://github.com/kiddos/malokai.vim.git ~/.vim/malokai.vim
 ln -s ~/.vim/malokai.vim/colors/malokai.vim ~/.vim.colors/malokai.vim
 cd ~
 wget https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
