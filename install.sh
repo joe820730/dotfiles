b8f288287f386df94645e399545995dcae2a94f0
Joe Tsai <joe.tsai8207@gmail.com>
2018-11-15 17:58:30 +0800

    Fix typo.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 mkdir ~/.vim
 mkdir ~/.vim/colors
 git clone https://github.com/kiddos/malokai.vim.git ~/.vim/malokai.vim
-ln -s ~/.vim/malokai.vim/colors/malokai.vim ~/.vim.colors/malokai.vim
+ln -s ~/.vim/malokai.vim/colors/malokai.vim ~/.vim/colors/malokai.vim
 cd ~
 wget https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
 mv git-prompt.sh .git-prompt.sh
