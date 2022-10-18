8d9e02a89185a0eb9b70cc6180671b42b552c0f6
Joe Tsai <joe.tsai8207@gmail.com>
2022-10-18 20:32:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,56 @@
 #!/bin/sh
-sudo apt install git vim tmux
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+is_root () {
+    return $(id -u)
+}
+
+has_sudo() {
+    local prompt
+
+    prompt=$(sudo -nv 2>&1)
+    if [ $? -eq 0 ]; then
+        echo "has_sudo__pass_set"
+        return 0
+    elif echo $prompt | grep -q '^sudo:'; then
+        echo "has_sudo__needs_pass"
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+has_sudo
+
+if [[ $? -eq 0 ]];
+then
+    echo "Install packages 'git vim tmux'"
+    sudo apt install git vim tmux;
+fi
+
+git clone https://github.com/ohmyzsh/oh-my-zsh.git ~/.oh-my-zsh
 mkdir ~/.vim
 mkdir ~/.vim/colors
 git clone https://github.com/kiddos/malokai.vim.git ~/.vim/malokai.vim
 ln -s ~/.vim/malokai.vim/colors/malokai.vim ~/.vim/colors/malokai.vim
-cd ~
 wget https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
-mv git-prompt.sh .git-prompt.sh
+mv git-prompt.sh ~/.git-prompt.sh
+if [[ -f $HOME/.bashrc ]]
+then
+    cp $HOME/.bashrc $HOME/.bashrc.bak
+    cp ./bashrc $HOME/.bashrc
+fi
+if [[ -f $HOME/.zshrc ]]
+then
+    cp $HOME/.zshrc $HOME/.zshrc.bak
+    cp ./zshrc $HOME/.zshrc
+fi
+if [[ -f $HOME/.vimrc ]]
+then
+    cp $HOME/.vimrc $HOME/.vimrc.bak
+    cp ./vimrc $HOME/.vimrc
+fi
+if [[ -f $HOME/.tmux.conf ]]
+then
+    cp $HOME/.tmux.conf $HOME/.tmux.conf.bak
+    cp ./tmux.conf $HOME/.tmux.conf
+fi
